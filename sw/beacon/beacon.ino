5f7b2be1d7c20d1fed017dc84e4b429d43503be9
ja <jaesparza@gmail.com>
2020-11-11 15:39:27 +0100

    Changed to use constructor and init function for the AD9850 class

--- sw/beacon/beacon.ino
+++ sw/beacon/beacon.ino
@@ -12,36 +12,39 @@
  *
  */
 
+#include "./src/QRSS.h"
 #include "./src/lib/AD9850.h"
 #include "HardwareConfig.h"
 #include <Arduino.h>
 
-AD9850 oscillator;
+AD9850 *oscillator;
+QRSS *messenger;
 
 uint32_t freq = 0;
 
 void setup() {
+
     BEACON_SERIAL.begin(115200);
-    initialized();
-    oscillator.begin(SELECT, RESET, FQUP);
+    oscillator = new AD9850(SPI_N, SPI_CLOCK, SELECT, RESET, FQUP);
+    oscillator->init();
+
+    messenger = new QRSS(oscillator);
     freq = 10000000;
-    oscillator.setFrequency(freq);
+    oscillator->setFrequency(freq);
+
+    BEACON_SERIAL.println("QRSS/WSPR Beacon initialized");
 }
 
 void loop() {}
 
-void initialized() {
-    BEACON_SERIAL.println("QRSS/WSPR Beacon init");
-}
-
 void testToneGeneration() {
     freq = 10140250 + 1.468;
     delay(1000);
-    oscillator.setFrequency(freq);
+    oscillator->setFrequency(freq);
     freq = freq + 1.468;
     delay(1000);
-    oscillator.setFrequency(freq);
+    oscillator->setFrequency(freq);
     freq = freq + 1.468;
     delay(1000);
-    oscillator.setFrequency(freq);
+    oscillator->setFrequency(freq);
 }
