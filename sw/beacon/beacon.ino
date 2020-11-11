a87e0f892a4cbcf6f8caa50deff0a60e2ddf4d93
ja <jaesparza@gmail.com>
2020-11-11 08:36:53 +0100

    Update beacon.ino

--- sw/beacon/beacon.ino
+++ sw/beacon/beacon.ino
@@ -1,3 +1,17 @@
+
+/*
+ * Filename: beacon.ino
+ * Description: main file for the beacon controller.
+ * Author: jaesparza - jaesparza@gmail.com
+ *
+ * Compiled with arduino IDE version 1.8.10, edited with VScode and formatted
+ * with with clang-format.
+ *
+ * Project: radio-beacon
+ * Hardware schematics: See HW folder in Github
+ *
+ */
+
 #include "./src/lib/AD9850.h"
 #include "HardwareConfig.h"
 #include <Arduino.h>
@@ -10,26 +24,24 @@ void setup() {
     BEACON_SERIAL.begin(115200);
     initialized();
     oscillator.begin(SELECT, RESET, FQUP);
-}
-
-void loop() {
-
-    /*
-    freq =    10140250 + 1.468;
-    delay(1000);
-    freqGen.setFrequency(freq);
-    freq = freq + 1.468;
-    delay(1000);
-    freqGen.setFrequency(freq);
-    freq = freq + 1.468;
-    delay(1000);
-    freqGen.setFrequency(freq);
-    */
-    // freq =  20000000;
     freq = 10000000;
     oscillator.setFrequency(freq);
 }
 
+void loop() {}
+
 void initialized() {
-    BEACON_SERIAL.println("QRSS Beacon");
+    BEACON_SERIAL.println("QRSS/WSPR Beacon init");
+}
+
+void testToneGeneration() {
+    freq = 10140250 + 1.468;
+    delay(1000);
+    oscillator.setFrequency(freq);
+    freq = freq + 1.468;
+    delay(1000);
+    oscillator.setFrequency(freq);
+    freq = freq + 1.468;
+    delay(1000);
+    oscillator.setFrequency(freq);
 }
