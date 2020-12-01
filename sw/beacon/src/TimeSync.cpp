24c46394f2b157b759d6ce2ac3378831da86b117
ja <jaesparza@gmail.com>
2020-12-01 20:55:45 +0100

    RTC sync prototype
    RTC syncing to GPS time has to be extracted to the TimeSync object later on.

--- sw/beacon/src/TimeSync.cpp
+++ sw/beacon/src/TimeSync.cpp
@@ -1,1 +1,5 @@
 #include "TimeSync.h"
+
+const char *weekdays[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+const char *months[] = {"Dummy", "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                        "Jul",   "Aug", "Sep", "Oct", "Nov", "Dec"};
